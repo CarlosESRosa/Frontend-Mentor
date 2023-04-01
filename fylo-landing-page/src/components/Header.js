878fe7918eb4857b4d2cdc43133bb42bf4080851
Carlos Rosa <carlosrosa1001@gmail.com>
2023-04-01 09:44:50 -0300

    feat: :sparkles: Header component

---
+++ fylo-landing-page/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <ul>
+        <li>
+          <img src={logo}></img>
+        </li>
+        <div>
+          <li>Features</li>
+          <li>Team</li>
+          <li>Sign In</li>
+        </div>
+      </ul>
+    </div>
+  );
+}
