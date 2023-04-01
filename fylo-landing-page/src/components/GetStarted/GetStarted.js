80d2450dfb7eace7c9af4fd651bd2c7bfb58b719
Carlos Rosa <carlosrosa1001@gmail.com>
2023-04-01 10:15:43 -0300

    feat: :sparkles: Get started component

--- fylo-landing-page/src/components/GetStarted/GetStarted.js
+++ fylo-landing-page/src/components/GetStarted/GetStarted.js
@@ -1,5 +1,21 @@
 import React from "react";
+import "./GetStarted.css";
+
+import ilustrationIntro from "../../images/illustration-intro.png";
 
 export default function GetStarted() {
-  return <div>GetStarted</div>;
+  return (
+    <div className="getStarted">
+      <img src={ilustrationIntro}></img>
+      <h1 className="main-title">
+        All your files in one secure location, accessible anywhere.
+      </h1>
+      <p className="basic-text">
+        Fylo stores all your most important files in one secure location. Access
+        them wherever you need, share and collaborate with friends family, and
+        co-workers.
+      </p>
+      <button className="get-started-button">Get Started</button>
+    </div>
+  );
 }
