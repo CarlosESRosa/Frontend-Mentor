bccc55d34467b87b086d6b3791fd23a54ee8184a
Carlos Rosa <carlosrosa1001@gmail.com>
2023-04-03 01:00:27 -0300

    feat: :sparkles: Done nft card project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import nftImage from "./images/image-equilibrium.jpg";
+import nftIcon from "./images/icon-ethereum.svg";
+import clockIcon from "./images/icon-clock.svg";
+import profileImg from "./images/image-avatar.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="nft-box">
+        <img src={nftImage} id="main-img"></img>
+        <h1>Equilibrium #3429</h1>
+        <p>Our Equilibrium collection promotes balance and calm.</p>
+        <div id="infos-section">
+          <div>
+            <img src={nftIcon}></img>
+            <span id="nft-value">0.041 ETH</span>
+          </div>
+          <div>
+            <img src={clockIcon}></img>
+            <span>3 3days left</span>
+          </div>
+        </div>
+        <div className="profile-infos">
+          <img src={profileImg}></img>
+          <p>Creation of</p>
+          <span>Jules Wyvern</span>
+        </div>
+      </div>
+    </main>
   );
 }
 
